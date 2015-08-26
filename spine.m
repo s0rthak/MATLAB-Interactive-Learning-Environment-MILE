3de62c6ac960b6777b4853080d349bd0ba912c12
Sarthak Batra <batrasoe@outlook.com>
2015-08-26 22:32:00 +0530

    Added modularity to basic YAML parsing

--- spine.m
+++ spine.m
@@ -4,19 +4,13 @@ yaml_file = 'text.yml';
 addpath(genpath('YAMLparser'));
 YamlStruct = ReadYaml(yaml_file);
 pause on;
-
-%Lacks Modularity here
+YamlStructNames = char(fieldnames(YamlStruct));
+%Added Modularity to this part
 obj = ExecuteFunctions;
-Task = LessonContent(YamlStruct.l1.id,YamlStruct.l1.content,YamlStruct.l1.type);
-obj.CheckObjectType(Task);
-Task = LessonContent(YamlStruct.l2.id,YamlStruct.l2.content,YamlStruct.l2.type);
-obj.CheckObjectType(Task);
-Task = LessonContent(YamlStruct.l3.id,YamlStruct.l3.content,YamlStruct.l3.type);
-obj.CheckObjectType(Task);
-Task = LessonContent(YamlStruct.l4.id,YamlStruct.l4.content,YamlStruct.l4.type);
-obj.CheckObjectType(Task);
-Task = LessonContent(YamlStruct.l5.id,YamlStruct.l5.content,YamlStruct.l5.type);
-obj.CheckObjectType(Task);
+for i = 1:length(YamlStructNames)
+    Task = LessonContent(YamlStruct.(YamlStructNames(i,:)).id,YamlStruct.(YamlStructNames(i,:)).content,YamlStruct.(YamlStructNames(i,:)).type);
+    obj.CheckObjectType(Task);
+end
 disp('End of Lesson!');
 
 
